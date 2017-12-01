40e4e23d607f5fedef1613a218da67cb58cfdb2b
Mario Cueva <mario.cueva.au@gmail.com>
2017-12-01 22:50:31 +1100

    working on timer states

--- js/index.js
+++ js/index.js
@@ -1,23 +1,32 @@
 $(document).ready(function () {
 
+    var state = {
+        "stopped": "stopped",
+        "running": "running",
+        "paused": "paused"
+    }
 
     var Timer = function () {
+        var timerState = state.stopped;
+        var counterID;
+        var cycleName;
 
-        var duration = 0;
-        var paused = null;
-        var start = false;
-        var counterID = undefined;
-
-        this.setDuration = (num) => duration = num;
-        this.getDuration = () => duration;
-        this.setPause = (val) => true;
-        this.isPaused = () => paused;
+        this.setTimerState = (val) => timerState = val;
+        this.getTimerState = () => timerState;
         this.getCounterID = () => counterID;
+        this.setCycleName = (val) => cycleName = val;
+        this.getCycleName = () => cycleName;
 
-        this.start = (xduration) => {
+        this.start = (duration, cycle) => {
             return new Promise(resolve => {
 
-                $('#countdown').text(xduration);
+                cycleName = cycle;
+                timerState = state.running;
+                $('#countdown').text(duration);
+
+                console.log('cycleName: ' + cycleName);
+                console.log('state: ' + timerState);
+                console.log('duration: ' + duration);
 
                 //decrenent the display every second
                 counterID = setInterval(function () {
@@ -35,21 +44,41 @@ $(document).ready(function () {
 
     var myTimer = new Timer();
 
-    myTimer.setDuration(9);
-    console.log(myTimer.getDuration());
-    console.log(myTimer.getCounterID());
-
-
     $('#start').click(function () {
-        // Timer($('#workValue').val()).then(() =>
-        //     Timer($('#restValue').val()));
-
-        myTimer.start($('#workValue').val()).then(() => myTimer.start($('#restValue').val()));
+        console.log('start clicked');
+        myTimer.start($('#workValue').val(), 'work').then(() =>
+            myTimer.start($('#restValue').val(), 'rest')).then(() => {
+                myTimer.setTimerState(state.stopped); 
+                console.log(myTimer.getTimerState());
+        });
     });
 
     $('#pause').click(function () {
-        console.log(myTimer.getCounterID());
+        console.log('pause clicked');
+
+        switch (myTimer.getTimerState()) {
+            case 'paused':
+                switch (myTimer.getCycleName()) {
+                    case 'work':
+                        myTimer.start($('#countdown').text(), 'work').then(() =>
+                            myTimer.start($('#restValue').val(), 'rest'));
+                        break;
+
+                    case 'rest':
+                        myTimer.start($('#countdown').text(), 'work')
+                }
+                myTimer.setTimerState('running');
+                break;
+
+            case 'running':
+                clearInterval(myTimer.getCounterID());
+                myTimer.setTimerState('paused');
+                console.log('state: ' + myTimer.getTimerState());
+                break;
+
+            case 'stopped':
+                console.log('state: ' + myTimer.getTimerState());
+                //nothing to pause. nothing to do
+        }
     });
 });
-
-
