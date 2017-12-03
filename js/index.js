0929382fe4c07de90c7599f4b2e5625ed9ac58ea
Mario Cueva <mario.cueva.au@gmail.com>
2017-12-03 20:52:50 +1100

    worked out pause and stop method on same thread as timer and promise. timer now working as required

--- js/index.js
+++ js/index.js
@@ -1,16 +1,18 @@
 $(document).ready(function () {
 
+    //state object just adds some consitency when assigning values (helps with typos)
     var state = {
         "stopped": "stopped",
         "running": "running",
         "paused": "paused"
     }
 
+    //cycle object just adds some consitency when assigning values (helps with typos)
     var cycle = {
         "work": "work",
         "rest": "rest"
     }
-
+    //timer object provides timing and screen updating operations
     var Timer = function () {
         //private properties
         var timerState = state.stopped;
@@ -26,43 +28,62 @@ $(document).ready(function () {
 
         //public methods
         this.start = (duration, cycle) => {
-
-            // console.log("GO");
             return new Promise((resolve, reject) => {
 
-                if (timerState !== state.running) {
+                //start the timer if its currently stopped
+                if (timerState === state.stopped) {
+
                     timerState = state.running;
                     cycleName = cycle;
 
-                    $('#countdown').text(duration);
+                    $('#countdown').text(duration); //temporary assignment until we figure out the UI ***********
 
                     console.log('cycleName: ' + cycleName);
                     console.log('duration: ' + duration);
                     console.log('state: ' + timerState);
 
-                    //decrenent the display every second
+                    //start the timer. 
+                    //Timer 'ticks' every second checking the following logic
                     counterID = setInterval(function () {
-                        if ($('#countdown').text() > 0) {
-                            $('#countdown').text($('#countdown').text() - 1);
-                        } else {
-                            clearInterval(counterID);
 
+                        //timer will run until the display reads zero
+                        if ($('#countdown').text() > 0) {
+
+                            switch (timerState) {
+
+                                //countdown the display while we're in 'running' state
+                                case state.running:
+                                    $('#countdown').text($('#countdown').text() - 1);
+                                    break;
+
+                                    //dont update the display if we're in paused state
+                                    //dummy case block added to make the logic easier to read.
+                                case state.paused:
+                                    break;
+
+                                    //if 'stopped'; end the timer and prematurely exit the promise
+                                case state.stopped:
+                                    clearInterval(counterID);
+                                    console.log('cycle STOPPED');
+                                    console.log('cycleName: ' + cycleName);
+                                    console.log('state: ' + timerState);
+                                    reject(new Error('Reset'));
+                            }
+                        } else {
+                            //once the display reaches zero, end the timer and resolve the promise
+                            clearInterval(counterID);
                             timerState = state.stopped;
-                            console.log('cycle finished');
+                            console.log('cycle Finished');
                             console.log('cycleName: ' + cycleName);
                             console.log('state: ' + timerState);
-
                             resolve();
                         }
                     }, 1000);
-                } else {
-                    console.log("timer is already running");
                 }
             });
         }
 
         this.stop = () => {
-            clearInterval(counterID);
             timerState = state.stopped;
             console.log('state: ' + timerState);
         }
@@ -70,18 +91,19 @@ $(document).ready(function () {
         this.pause = () => {
             switch (timerState) {
                 case 'paused':
-                    //resume timer if timer is paused
-                    this.start($('#countdown').text(), cycleName);
+                    //resume if paused
+                    timerState = state.running;
+                    console.log('state: ' + timerState);
                     break;
 
                 case 'running':
-                    //pause if timer is currently running
-                    clearInterval(counterID);
+                    //pause if running
                     timerState = state.paused;
                     console.log('state: ' + timerState);
                     break;
 
                 case 'stopped':
+                    //do nothing if stopped
                     console.log('state: ' + timerState);
                     console.log('Nothing to pause because timer is stopped.');
             }
@@ -93,14 +115,16 @@ $(document).ready(function () {
     $('#start').click(function () {
         console.log('start clicked');
 
-        myTimer.start($('#workValue').val(), cycle.work).then(() =>
-            myTimer.start($('#restValue').val(), cycle.rest)).then(() => {
-            myTimer.setTimerState(state.stopped);
-            console.log(myTimer.getTimerState());
-        });
-
-
-
+        myTimer.start($('#workValue').val(), cycle.work)
+            .then(() =>
+                myTimer.start($('#restValue').val(), cycle.rest))
+            .then(() => {
+                myTimer.setTimerState(state.stopped);
+                console.log(myTimer.getTimerState());
+            })
+            .catch((error) => {
+                console.log(error.message);
+            });
 
     });
 
@@ -110,7 +134,7 @@ $(document).ready(function () {
     });
 
     $('#reset').click(function () {
-        console.log('reset clicked - but we are going to just STOP');
+        console.log('reset clicked - but we are going to just STOP for now');
         myTimer.stop();
     });
 });
